d4701e68159457e604a6947a946572dbd429f928
Kseniya Baluk <kseniya.baluk@broadsign.com>
2022-04-30 04:42:45 -0400

    Add saucer animation

--- src/UI/Saucer/Saucer.test.js
+++ src/UI/Saucer/Saucer.test.js
@@ -3,28 +3,36 @@ import { screen, render, fireEvent } from '@testing-library/react';
 import Saucer from './Saucer';
 
 describe('<Saucer> component', () => {
+    let onClick;
+
+    beforeEach(() => {
+        onClick = jest.fn();
+    });
     it('should render saucer', () => {
-        render(<Saucer testId="saucer-1" />);
+        render(<Saucer testId="saucer-1" id={1} onClick={onClick} />);
         expect(screen.getByTestId('saucer-1')).toBeInTheDocument();
     });
 
     it('should render saucer head', () => {
-        render(<Saucer testId="saucer" />);
-        expect(screen.getByTestId('saucer-head')).toBeInTheDocument();
+        render(<Saucer testId="saucer" id={1} onClick={onClick} />);
+        const head = screen.getByTestId('saucer-head');
+        expect(head).toBeInTheDocument();
+        expect(head.classList).toContain('head');
     });
 
     it('should render saucer body', () => {
-        render(<Saucer testId="saucer" />);
-        expect(screen.getByTestId('saucer-body')).toBeInTheDocument();
+        render(<Saucer testId="saucer" id={1} onClick={onClick} />);
+        const body = screen.getByTestId('saucer-body');
+        expect(body).toBeInTheDocument();
+        expect(body.classList).toContain('body');
     });
 
     it('should render 5 saucer windows', () => {
-        render(<Saucer testId="saucer" />);
+        render(<Saucer testId="saucer" id={1} onClick={onClick} />);
         expect(screen.getAllByTestId('saucer-window').length).toEqual(5);
     });
 
     it('should pass its id back when clicked', () => {
-        const onClick = jest.fn();
         render(<Saucer testId="saucer" onClick={onClick} id={3} />);
         const saucer = screen.getByTestId('saucer');
         fireEvent.click(saucer);
--- src/UI/Saucer/Saucer.tsx
+++ src/UI/Saucer/Saucer.tsx
@@ -10,14 +10,16 @@ interface ISaucerProps {
 const Saucer: FC<ISaucerProps> = ({ testId, id, onClick }: ISaucerProps): JSX.Element => {
     const renderWindows = (num: number): JSX.Element[] => {
         const mapper = Array.from(Array(num).keys());
-        return mapper.map((m) => <span data-testid={`${testId}-window`} key={m} />);
+        return mapper.map((m) => (
+            <span className={s.window} data-testid={`${testId}-window`} key={m} />
+        ));
     };
 
     return (
         <button data-testid={testId} onClick={() => onClick(id)} className={s.wrapper}>
             <span data-testid={`${testId}-head`} className={s.head} />
             <span data-testid={`${testId}-body`} className={s.body} />
-            <div>{renderWindows(5)}</div>
+            <div className={s.windows}>{renderWindows(5)}</div>
         </button>
     );
 };
